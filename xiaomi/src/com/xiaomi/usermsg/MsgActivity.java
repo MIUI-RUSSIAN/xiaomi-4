c7389c93a60dfa74a7c7c93e698f75452b3d18cc
paozao <870251445@qq.com>
2014-05-14 17:34:20 +0800

    Signed-off-by: paozao <870251445@qq.com>

---
+++ xiaomi/src/com/xiaomi/usermsg/MsgActivity.java
@@ -0,0 +1,16 @@
+package com.xiaomi.usermsg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.xiaomi.R;
+
+public class MsgActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_msg);
+
+	}
+}
